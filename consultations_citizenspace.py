41eb252c6609ddf81645b673614d1394264bc5d1
Paul Bradshaw <paulonhismobile@gmail.com>
2019-04-10 11:33:17 +0100

    Update consultations_citizenspace.py

--- consultations_citizenspace.py
+++ consultations_citizenspace.py
@@ -8,6 +8,7 @@ import urllib2
 
 #For testing
 testurl = "https://sheffield.citizenspace.com/"
+aggurl = "https://aggregator.delib.net/citizenspace/?state_filter=closed&search_text=&region=&source=&custom_sort=newest_update&per_page=500&page=033"
 closedconsultations = "consultation_finder/?sort_on=iconsultable_enddate&sort_order=descending&advanced=1&st=closed#cs-finder-results-container"
 #Change this when doing other urls
 url = testurl+closedconsultations
